ccdf0d4027c25f0e533e3f53b0deee220e51bb3f
Michael Nash <mnash@jglobal.com>
2013-09-17 15:54:49 -0600

    Initial flow of messages through the proxy to the server in place

--- client/src/main/scala/com/jglobal/tardis/TardisProxy.scala
+++ client/src/main/scala/com/jglobal/tardis/TardisProxy.scala
@@ -6,20 +6,41 @@ import scala.collection.mutable.HashMap
 import scala.collection.mutable.SynchronizedMap
 
 import akka.actor._
+import com.typesafe.config._
 
-class TardisProxy(bus: ActorRef) {
-  val handlers = new collection.mutable.HashMap[String, (EventContainer) => Unit] with SynchronizedMap[String, (EventContainer) => Unit]
+object TardisProxy {
+  def apply(clientId: String) = {
+    val system = ActorSystem("client", ConfigFactory.load("client"))
+    val bus = system.actorSelection("akka.tcp://tardis@127.0.0.1:9999/user/eventrouter")
+    val proxyActor = system.actorOf(TardisProxyActor.props(bus), "busProxy")
+    new TardisProxy(clientId, bus, proxyActor)
+  }
+}
 
-  def publish(evt: EventContainer, confirm: (Ack) => Unit) {}
+class TardisProxy(clientId: String, bus: ActorSelection, proxyActor: ActorRef) {
+
+   val handlers = new collection.mutable.HashMap[String, (EventContainer) => Unit] with SynchronizedMap[String, (EventContainer) => Unit]
+
+  def publish(evt: EventContainer, confirm: (Ack) => Unit) {
+    proxyActor ! SendEvent(evt)
+  }
   
   def registerHandler(handler: (EventContainer) => Unit, eventType: String) {
     handlers.put(eventType, handler)
+    proxyActor ! Subscription(clientId, handlers.keys.toList)
   }
   def ack(id: UUID) {}
 }
 
-class TardisProxyActor(bus: ActorRef) extends Actor with ActorLogging {
+object TardisProxyActor {
+  def props(bus: ActorSelection): Props = Props(classOf[TardisProxyActor], bus)
+}
+
+case class SendEvent(container: EventContainer)
+
+class TardisProxyActor(bus: ActorSelection) extends Actor with ActorLogging {
   def receive = {
+    case SendEvent(container) => bus ! container
     case _ => {}
   }
 }
--- client/src/test/scala/com/jglobal/tardis/TardisProxyTest.scala
+++ client/src/test/scala/com/jglobal/tardis/TardisProxyTest.scala
@@ -4,16 +4,23 @@ import org.scalatest.FreeSpec
 import akka.actor._
 import com.typesafe.config._
 import akka.testkit._
+import collection.mutable.ListBuffer
 
 class TardisProxyTest(system: ActorSystem) extends TestKit(system) with FreeSpec {
   def this() = this(ActorSystem("test-proxy", ConfigFactory.load("client").getConfig("test")))
 
   implicit val implSys = system
-  
-  val busStub = TestActorRef(Props[TestActor])
+  val received = collection.mutable.ListBuffer[Any]()
+
+  val busRef = system.actorOf(TestActor.props(received))
+  val busStub = ActorSelection(busRef, busRef.path.toString)
   
   "the tardis proxy" - {
     "when a handler is registered, add the type to the list of subscribed types sent to the server" in {
+      val proxy = new TardisProxy("clientId", busStub, system.actorOf(TardisProxyActor.props(busStub)))
+      val eventType = "foo"
+      proxy.registerHandler((e: EventContainer) => {}, eventType)
+      awaitAssert(received.size === 1)
     }
     "when publishing an event, sends the event to the server" in {
     }
@@ -26,16 +33,12 @@ class TardisProxyTest(system: ActorSystem) extends TestKit(system) with FreeSpec
   }
 }
 
-trait Recording {
-  val received = collection.mutable.ListBuffer[EventContainer]()
+object TestActor {
+  def props(received: ListBuffer[Any]) = Props(classOf[TestActor], received)
 }
 
-class TestActor extends Actor with Recording {
+class TestActor(received: ListBuffer[Any]) extends Actor {
   def receive = {
-    case container: EventContainer => {
-      received.append(container)
-      assert(received.size > 0)
-    }
-    case _ => throw new IllegalArgumentException("Unknown message")
+    case x: Any => received.append(x)
   }
 }
