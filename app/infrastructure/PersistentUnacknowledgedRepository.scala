a7ef606c4761aaa2b88dde5c5c66d408b0f7be12
Michael Nash <mnash@jglobal.com>
2013-09-26 07:22:46 -0600

    Checkpoint - working on tests for persistent unack repo

--- app/infrastructure/PersistentUnacknowledgedRepository.scala
+++ app/infrastructure/PersistentUnacknowledgedRepository.scala
@@ -3,8 +3,10 @@ package infrastructure
 import domain._
 import akka.actor._
 import infrastructure.api._
+import com.jglobal.tardis.EventContainer
 
 import java.io.File
+import java.io.FileOutputStream
 import java.util.UUID
 
 class PersistentUnacknowledgedRepository(path: String, clientRepo: ClientRepository, eventRepository: EventRepository, system: ActorSystem) extends UnacknowledgedRepository(clientRepo, system) {
@@ -20,7 +22,9 @@ class PersistentUnacknowledgedRepository(path: String, clientRepo: ClientReposit
 
     unackDir.listFiles().toList.filter(_.isDirectory).foreach(clientDir => {
       clientDir.listFiles().toList.filter(_.isFile).foreach(eventFile => {
-        super.store(ClientIdAndEventId(clientDir.getName, UUID.fromString(eventFile.getName)), EventContainerAndTimeStamp(eventRepository.find(UUID.fromString(eventFile.getName)).get, eventFile.lastModified))
+        val correspondingEvent: EventContainer = eventRepository.find(UUID.fromString(eventFile.getName)).getOrElse(throw new IllegalArgumentException(s"No such event stored with id ${eventFile.getName}"))
+        super.store(ClientIdAndEventId(clientDir.getName, UUID.fromString(eventFile.getName)),
+          EventContainerAndTimeStamp(correspondingEvent, eventFile.lastModified))
       })
     })
   }
@@ -36,8 +40,10 @@ class PersistentUnacknowledgedRepository(path: String, clientRepo: ClientReposit
     val dir = new File(s"${unackDir.getPath}/${clientAndEventId.clientId}")
     if (!dir.exists) dir.mkdirs()
     val unackFile = new File(s"${dir.getPath}/${clientAndEventId.eventId}")
+    new FileOutputStream(unackFile).close()
     unackFile.setLastModified(System.currentTimeMillis)
   }
+
 }
 
 
--- test/infrastructure/PersistentUnacknowledgedRepositoryTest.scala
+++ test/infrastructure/PersistentUnacknowledgedRepositoryTest.scala
@@ -5,9 +5,21 @@ import org.scalatest.FreeSpec
 import com.jglobal.tardis._
 import java.util.UUID
 import infrastructure.api._
+import java.io.File
 
-class PersistentUnacknowledgerRepositoryTest extends FreeSpec {
-  val path = "/tmp"
+trait PersistentRepositoryTest {
+  def deleteRecursively(dir: File): Boolean = {
+    if (dir.isDirectory) dir.listFiles match {
+      case null =>
+      case xs  => xs foreach deleteRecursively
+    }
+    dir.delete()
+  }
+}
+
+class PersistentUnacknowledgedRepositoryTest extends FreeSpec with PersistentRepositoryTest {
+  val path = "/tmp/test"
+  deleteRecursively(new File(path))
   "the persistent unacknowledged repository" - {
     "can store and retrieve information for an unacknowledged event to a client" in {
       val eventRepo = new EventRepository
@@ -18,13 +30,37 @@ class PersistentUnacknowledgerRepositoryTest extends FreeSpec {
       repo.store(clientAndEventId, containerAndTimeStamp)
       val result = repo.dueForRetry.toList
       assert(result.size === 1)
-    }
-    "can list all information for unacknowledged events that are due for retry" in {
+      assert(result.head._1.id === "clientId")
+      assert(result.head._2 === event)
     }
     "can remove information for an event once it's acknowledged" in {
+      val eventRepo = new EventRepository
+      val repo = new PersistentUnacknowledgedRepository(path, new TransientClientRepository, eventRepo, null)
+      val event = EventContainer(UUID.randomUUID, "type", "payload", "clientId")
+      val clientAndEventId = ClientIdAndEventId("clientId", event.id)
+      val containerAndTimeStamp = EventContainerAndTimeStamp(event, System.currentTimeMillis - 40000)
+      repo.store(clientAndEventId, containerAndTimeStamp)
+      repo.remove(clientAndEventId)
+      assert(repo.dueForRetry.size === 0)
     }
     "can store unacknowledged data from one instantiation to another" in {
+      val eventRepo = new PersistentEventRepository(path)
+      val clientRepo = new PersistentClientRepository(path, null)
+      val repo = new PersistentUnacknowledgedRepository(path, clientRepo, eventRepo, null)
+      val event = EventContainer(UUID.randomUUID, "type", "payload", "clientId")
+      eventRepo.store(event)
+      val clientAndEventId = ClientIdAndEventId("clientId", event.id)
+      val containerAndTimeStamp = EventContainerAndTimeStamp(event, System.currentTimeMillis - 40000)
+      repo.store(clientAndEventId, containerAndTimeStamp)
       
+      val eventRepo2 = new PersistentEventRepository(path)
+      val clientRepo2 = new PersistentClientRepository(path, null)
+      val repo2 = new PersistentUnacknowledgedRepository(path, clientRepo2, eventRepo2, null)
+
+      val result = repo2.dueForRetry.toList
+      assert(result.size === 1)
+      assert(result.head._1.id === "clientId")
+      assert(result.head._2 === event)      
     }
   }
 }
