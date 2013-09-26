f38482a980a32e60c884d29f5d474384828c27af
Michael Nash <mnash@jglobal.com>
2013-09-26 16:46:26 -0600

    Clean up test, DRY out setup

--- test/api/EventRouterTest.scala
+++ test/api/EventRouterTest.scala
@@ -16,15 +16,19 @@ class EventRouterTest(system: ActorSystem) extends TestKit(system) with FreeSpec
   def this() = this(ActorSystem("test-router", ConfigFactory.load().getConfig("test")))
    
   implicit val implSys = system
+
+  private[this] def transientRouter = {
+    val clientRepo = new TransientClientRepository
+    val subscriptionActor = TestActorRef(new SubscriptionActor(clientRepo))
+    val unacknowledgedRepo = new UnacknowledgedRepository(clientRepo, system)
+    val eventRepo = new EventRepository
+    (clientRepo, subscriptionActor, unacknowledgedRepo, eventRepo, TestActorRef(new EventRouterActor(subscriptionActor, clientRepo, unacknowledgedRepo, eventRepo)))
+  }
   
   "the event router class" - {
     "when receiving a subscription message" - {
       "updates the appropriate client in the client repository" in {
-        val clientRepo = new TransientClientRepository
-        val subscriptionActor = TestActorRef(new SubscriptionActor(clientRepo))
-        val unacknowledgedRepo = new UnacknowledgedRepository(clientRepo, system)
-        val eventRepo = new EventRepository
-        val router = TestActorRef(new EventRouterActor(subscriptionActor, clientRepo, unacknowledgedRepo, eventRepo))
+        val (clientRepo, _, _, _, router) = transientRouter
         val id = "someId"
         val eventType = "someType"
         router ! Subscription(id, List(eventType))
@@ -34,11 +38,7 @@ class EventRouterTest(system: ActorSystem) extends TestKit(system) with FreeSpec
     }
     "when receiving a published event" - {
       "records stats for the client indicating another event has been received" in {
-        val clientRepo = new TransientClientRepository
-        val subscriptionActor = TestActorRef(new SubscriptionActor(clientRepo))
-        val unacknowledgedRepo = new UnacknowledgedRepository(clientRepo, system)
-        val eventRepo = new EventRepository
-        val router = TestActorRef(new EventRouterActor(subscriptionActor, clientRepo, unacknowledgedRepo, eventRepo))
+        val (clientRepo, _, _, _, router) = transientRouter
         val event = EventContainer(UUID.randomUUID, "type", "payload", "someId")
         router ! event
         expectMsg(Ack(event.id, event.clientId))
@@ -68,6 +68,13 @@ class EventRouterTest(system: ActorSystem) extends TestKit(system) with FreeSpec
         val event = EventContainer(UUID.randomUUID, eventType, "payload", id)
         router ! event
         expectMsg(Ack(event.id, event.clientId))
+      }
+    }
+
+    "when receiving an ack from a client" - {
+      "removes the event from the list of unacknowledged events from that client" in {
+      }
+      "updates the client stats to indicate another ack has been received" in {
       }
     }
   }
